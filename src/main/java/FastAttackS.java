c46ce2451d018a3a0d9cfee9bb8c8271a904cb0a
Jeff Lent <jefflent01@gmail.com>
2019-01-16 22:26:57 -0600

    initial commit

---
+++ src/main/java/FastAttackS.java
@@ -0,0 +1,70 @@
+
+package com.galvanize.streams;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "Name",
+    "Type",
+    "Damage"
+})
+public class FastAttackS {
+
+    @JsonProperty("Name")
+    private String name;
+    @JsonProperty("Type")
+    private String type;
+    @JsonProperty("Damage")
+    private Integer damage;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("Name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("Name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("Type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("Type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("Damage")
+    public Integer getDamage() {
+        return damage;
+    }
+
+    @JsonProperty("Damage")
+    public void setDamage(Integer damage) {
+        this.damage = damage;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
